4f324d5f38586b101e872e82fc585c0aa2169700
Hironsan <light.tree.1.13@gmail.com>
2018-08-21 16:43:14 +0900

    Implement sentiment analysis

---
+++ examples/sentiment_analysis.py
@@ -0,0 +1,26 @@
+import argparse
+
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+
+def main(text):
+    client = language.LanguageServiceClient()
+
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT)
+
+    # Detects the sentiment of the text
+    sentiment = client.analyze_sentiment(document=document).document_sentiment
+
+    print('Text: {}'.format(text))
+    print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('text', help='The text you\'d like to analyze sentiment.')
+    args = parser.parse_args()
+    main(args.text)
